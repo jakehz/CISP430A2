46663831f32d1896a56e9cbe5f752b2f786ef32d
Jake Hernandez <jakejhdz@yahoo.com>
2018-10-28 00:24:52 +0000

    added a resize function

--- sequence2.cpp
+++ sequence2.cpp
@@ -38,6 +38,23 @@ void sequence::insert(const value_type& entry) {
 	++used;
 }
 
+void sequence::resize(size_type new_capacity){
+	if(new_capacity > used){
+		// allocate a new array with the capacity specified in the parameter.
+		value_type* larger_data = new value_type[new_capacity];
+		// put each datum into the new, larger array
+		for(size_t i = 0; i < used; i++){
+			larger_data[i] = data[i];
+		}
+		// delete the data from the original array.
+		delete [] data;
+		// assign the array pointer to the new, larger array.
+		data = larger_data;
+		// set the capacity to the new size
+		capacity = new_capacity;
+	}
+}
+
 void sequence::attach(const value_type& entry) {
 	if (current_index == used) 
 	{
